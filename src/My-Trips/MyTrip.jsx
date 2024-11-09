e8a8aa4b494dfed9f2fed3a933d96108c8762393
Jatin Gupta <jatin04gupta2004@gmail.com>
2024-11-09 22:51:33 +0530

    fx

--- src/My-Trips/MyTrip.jsx
+++ src/My-Trips/MyTrip.jsx
@@ -12,39 +12,42 @@ const MyTrip = () => {
   }, []);
   const GetUserTrips = async () => {
     const user = JSON.parse(localStorage.getItem("user"));
-    if (!user) {
+    if (!user && window.location.pathname != "/") {
+      navigate("/");
+      return;
+    } else if (!user) {
       navigate("/");
       return;
     }
     const q = query(
-        collection(db, "AITrips"),
-        where("userEmail", "==", user?.email)
+      collection(db, "AITrips"),
+      where("userEmail", "==", user?.email)
     );
     const querySnapshot = await getDocs(q);
-    setUserTrips([])
+    setUserTrips([]);
     querySnapshot.forEach((doc) => {
-    //   console.log(doc.id, "=>", doc.data());
-      setUserTrips(prevVal=>[...prevVal,doc.data()])
+      //   console.log(doc.id, "=>", doc.data());
+      setUserTrips((prevVal) => [...prevVal, doc.data()]);
     });
   };
 
   return (
     <div className="sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10">
-        <h2 className="font-bold text-3xl">MyTrips</h2>
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-5 mt-10">
-            {
-               userTrips.length>0 ?  userTrips.map((trip,index)=>(
-                    <UserTripCard trip={trip} key={index}/>
-                )) : 
-                [1,2,3,4,5,6].map((item,index)=>(
-                    <div key={index} className="h-[200px] w-full bg-slate-200 animate-pulse rounded-xl">
-
-                    </div>
-                ))
-            }
-        </div>
+      <h2 className="font-bold text-3xl">MyTrips</h2>
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-5 mt-10">
+        {userTrips.length > 0
+          ? userTrips.map((trip, index) => (
+              <UserTripCard trip={trip} key={index} />
+            ))
+          : [1, 2, 3, 4, 5, 6].map((item, index) => (
+              <div
+                key={index}
+                className="h-[200px] w-full bg-slate-200 animate-pulse rounded-xl"
+              ></div>
+            ))}
+      </div>
     </div>
-    );
+  );
 };
 
 export default MyTrip;
